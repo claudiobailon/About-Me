ffa0179920ce034114859e13e079ac854f1c1c34
claudiobailon <c.bailonschubert@gmail.com>
2020-06-02 18:04:10 -0700

    Add outline of js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 'use strict';
 
-console.log('it worked');
+//console.log('it worked');
+/*
+ getting to know me
+ 5 questions
+
+Let's play a game! I'll ask you a few yes or no questions about me and we'll see how many you can guess correctly! Here we go
+ Have I ever been to Europe?
+ A: No
+ if correct "Correct! I was actually planning a trip there for later this year, but COVID 19 derailed that:("
+ if false "Nope. "I was actually planning a trip there for later this year, but COVID 19 derailed that:("
+ 
+ Do I know how to play an instrument?
+ A Yes
+ if correct "Correct! I can play the saxaphone and was in jazz band in high school"
+ if false  "Wrong, I can actually play the saxaphone and was in jass band in high school"
+
+ Have I ever ran a marathon?
+ A No
+ if correct " You guessed it! Though maybe some day I'll work up the endurance"
+ if false " No, but thanks for believing in me!"
+
+ Am I afraid of bears?
+ a no
+ if correct "Correct. Well, its not a phobia, they're actually my favorite animal, but if i came across one in the wild i'd be a little, um, concerned"
+ if false Wrong!. Well, its not a phobia, they're actually my favorite animal, but if i came across one in the wild i'd be a little, um, concerned"
+
+ Do I have pets?
+  a yes
+  if correct (Indeed! My wife and I hace two cats named Blue and Ollie:)
+  if false (Incorrect.  )
+ */
+
+
+
+
